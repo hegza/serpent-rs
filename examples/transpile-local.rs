86747f6d204e3b6d5d79ca3429cf8a867d188d6d
Henri Lunnikivi <henri.lunnikivi@gmail.com>
2020-03-28 15:53:53 +0200

    Add two simple examples

---
+++ examples/transpile-local.rs
@@ -0,0 +1,15 @@
+use serpent::transpile;
+use sourcefile::SourceFile;
+
+fn main() {
+    let source_file = {
+        let filename = "examples/input/really-simple.py";
+        let sf = SourceFile::default();
+        sf.add_file(filename).unwrap()
+    };
+    let source = source_file.contents;
+
+    println!("Source:\n{}", &source);
+    let result = transpile(&source);
+    println!("Result:\n{}", &result);
+}
