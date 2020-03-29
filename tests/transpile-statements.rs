3ebd44ae8a66c7d26f56b099b1d6c0c4977a6385
Henri Lunnikivi <henri.lunnikivi@gmail.com>
2020-03-30 00:18:05 +0300

    Implement multiline test

--- tests/transpile-statements.rs
+++ tests/transpile-statements.rs
@@ -2,11 +2,18 @@ use serpent::transpile;
 
 #[test]
 fn transpile_local() {
-    env_logger::init();
-
     let stmt = "a = 5";
 
     let transpiled = transpile(stmt);
 
-    assert_eq!(transpiled, "let a = 5 ;");
+    assert_eq!(transpiled, "let a = 5;\n");
+}
+
+#[test]
+fn transpile_multiline() {
+    let py = include_str!("input/simple_multiline.py");
+
+    let transpiled = transpile(py);
+
+    assert_eq!(transpiled, include_str!("input/simple_multiline.rs"));
 }
