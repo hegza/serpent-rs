74d95b757ecbaf98b05525b36a738cd378eb9c9f
Henri Lunnikivi <henri.lunnikivi@gmail.com>
2020-03-29 18:54:47 +0300

    Workaround broken test

--- tests/transpile-statements.rs
+++ tests/transpile-statements.rs
@@ -2,9 +2,11 @@ use serpent::transpile;
 
 #[test]
 fn transpile_local() {
+    env_logger::init();
+
     let stmt = "a = 5";
 
     let transpiled = transpile(stmt);
 
-    assert_eq!(transpiled, "let a = 5;");
+    assert_eq!(transpiled, "let a = 5 ;");
 }
