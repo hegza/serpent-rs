8ed55a6d7f0f09bb559e8008d02dd04257cc3332
Henri Lunnikivi <henri.lunnikivi@gmail.com>
2020-03-29 18:54:34 +0300

    Add rudimentary logging

--- src/lib.rs
+++ src/lib.rs
@@ -18,7 +18,11 @@ pub fn transpile(python_source: &str) -> String {
 
     rust_program
         .iter()
-        .map(|stmt| stmt.0.to_token_stream().to_string())
+        .map(|stmt| {
+            let stream = stmt.0.to_token_stream();
+            println!("token: {:?}", &stream);
+            stream.to_string()
+        })
         .fold(String::new(), |acc, next| acc + &next)
 }
 
@@ -36,19 +40,22 @@ mod transpiler {
     use num_bigint::BigInt;
     use rustpython_parser::ast::*;
     use syn as rs_ast;
+    use log::{trace, debug};
 
     pub fn visit_statement(stmt: Statement) -> rs_ast::Stmt {
         let location = stmt.location;
         let stmt = stmt.node;
+        trace!("visit: {:?}", stmt);
 
         use StatementType::*;
         match stmt {
-            Assign { targets, value } => visit_assign(targets, value),
+            Assign { targets, value } => visit_assign(&targets, &value),
             _ => unimplemented!(),
         }
     }
 
-    fn visit_assign(targets: Vec<Expression>, value: Expression) -> rs_ast::Stmt {
+    fn visit_assign(targets: &[Expression], value: &Expression) -> rs_ast::Stmt {
+        trace!("visit: Assign({:?}, {:?})", targets, value);
         let lhs_target = if targets.len() == 1 {
             let target = targets.first().unwrap();
             let location = &target.location;
@@ -77,6 +84,7 @@ mod transpiler {
         });
 
         // TODO: Python 'assign' could be a Local or an assignment expression in Rust
+        // HACK: Used Local for now
         let local = rs_ast::Local {
             attrs: Vec::new(),
             let_token: <rs_ast::Token![let]>::default(),
@@ -87,29 +95,38 @@ mod transpiler {
             )),
             semi_token: <rs_ast::Token![;]>::default(),
         };
-        rs_ast::Stmt::Local(local)
+        let local = rs_ast::Stmt::Local(local);
+        debug!("Assign({:?}, {:?}) -> {:?}", &targets, &value, &local);
+        local
     }
 
-    fn visit_expression(expr: Expression) -> rs_ast::Expr {
-        let location = expr.location;
-        let expr = expr.node;
+    fn visit_expression(expr: &Expression) -> rs_ast::Expr {
+        trace!("visit: {:?}", expr);
+        let location = &expr.location;
+        let expr = &expr.node;
 
         use ExpressionType::*;
-        match expr {
-            Number { value } => visit_number(value),
+        let rs_expr = match expr {
+            Number { value } => visit_number(&value),
             _ => unimplemented!(),
-        }
+        };
+        debug!("{:?} -> {:?}", &expr, &rs_expr);
+        rs_expr
     }
 
-    fn visit_number(number: Number) -> rs_ast::Expr {
+    fn visit_number(number: &Number) -> rs_ast::Expr {
+        trace!("visit: {:?}", number);
         use Number::*;
-        match number {
-            Integer { value } => visit_bigint(value),
+        let rs_number = match number {
+            Integer { value } => visit_bigint(&value),
             _ => unimplemented!(),
-        }
+        };
+        debug!("{:?} -> {:?}", &number, rs_number);
+        rs_number
     }
 
-    fn visit_bigint(bigint: BigInt) -> rs_ast::Expr {
+    fn visit_bigint(bigint: &BigInt) -> rs_ast::Expr {
+        trace!("visit: {:?}", bigint);
         let (sign, data) = bigint.to_u32_digits();
         let value: u32 = {
             if data.len() == 1 {
@@ -118,7 +135,7 @@ mod transpiler {
                 unimplemented!()
             }
         };
-        match sign {
+        let expr = match sign {
             num_bigint::Sign::Plus => {
                 let literal = proc_macro2::Literal::u32_unsuffixed(value);
                 rs_ast::Expr::Lit(rs_ast::ExprLit {
@@ -127,6 +144,8 @@ mod transpiler {
                 })
             }
             _ => unimplemented!(),
-        }
+        };
+        debug!("{:?} -> {:?}", bigint, expr);
+        expr
     }
 }
