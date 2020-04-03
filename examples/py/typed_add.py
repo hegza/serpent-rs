38ef5c304578d6321e15f5dbd8145a88f06a8522
Henri Lunnikivi <henri.lunnikivi@gmail.com>
2020-04-03 22:10:59 +0300

    Add transpilation example 'typed_add'

---
+++ examples/py/typed_add.py
@@ -0,0 +1,6 @@
+def add(a: int, b: int) -> int:
+    return a + b
+
+
+c = 3
+print(add(2, c))
---
+++ examples/rs/typed_add.rs
@@ -0,0 +1,8 @@
+pub fn add(a: i32, b: i32) -> i32 {
+    a + b
+}
+
+fn main() {
+    let c = 3;
+    println!("{}", add(2, c));
+}
