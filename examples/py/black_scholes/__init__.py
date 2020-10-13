4b3347fa9f48b9fc9bdaf0df56fa516b11b48989
Henri Lunnikivi <henri.lunnikivi@gmail.com>
2020-10-13 17:58:36 +0300

    Simplify & bench bs for demo

--- examples/py/black_scholes/__init__.py
+++ examples/py/black_scholes/__init__.py
@@ -1,24 +1,32 @@
 # External imports
 import numpy as np
-import scipy.stats as si
+import csv
+import pandas
+import pandas as pd
+from time import time
 
-from black_scholes_ndp import euro_vanilla_put
+from black_scholes import euro_vanilla_put, euro_vanilla_call
 
 
 def main():
-    put = euro_vanilla_put(50, 100, 1, 0.05, 0.25)
-    print("put: " + str(put))
-
-    n = 100
-    S = np.random.rand(n) * 50 + 25
-    K = np.random.rand(n) * 100 + 50
-    T = np.random.rand(n) * 1 + 0.5
-    r = np.random.rand(n) * 0.05 + 0.025
-    sigma = np.random.rand(n) * 0.25 + 0.175
-
-    result = euro_vanilla_put(S, K, T, r, sigma)
-    print("result: " + str(result))
+    dataset = pd.read_csv('dataset.csv', sep=',', header=None).to_numpy()
+    
+    n = dataset.shape[0]
+    S = dataset[:,0]
+    K = dataset[:,1]
+    T = dataset[:,2]
+    r = dataset[:,3]
+    sigma = dataset[:,4]
+    
+    start_time = time()
+    put = euro_vanilla_put(S, K, T, r, sigma)
+    call = euro_vanilla_call(S, K, T, r, sigma)
+    duration = time() - start_time
+    
+    print("Calculating put and call for", n, "options took", duration, "seconds")
 
 
 if __name__ == "__main__":
     main()
+
+
---
+++ examples/py/black_scholes/black_scholes.py
@@ -0,0 +1,36 @@
+import numpy as np
+import scipy.stats as si
+
+
+def euro_vanilla_put(S, K, T, r, sigma):
+
+    # S: spot price
+    # K: strike price
+    # T: time to maturity
+    # r: interest rate
+    # sigma: volatility of underlying asset
+
+    d1 = (np.log(S / K) + (r + 0.5 * sigma ** 2) * T) / (sigma * np.sqrt(T))
+    d2 = (np.log(S / K) + (r - 0.5 * sigma ** 2) * T) / (sigma * np.sqrt(T))
+
+    put = (K * np.exp(-r * T) * si.norm.cdf(-d2, 0.0, 1.0) -
+           S * si.norm.cdf(-d1, 0.0, 1.0))
+
+    return put
+
+
+def euro_vanilla_call(S, K, T, r, sigma):
+
+    # S: spot price
+    # K: strike price
+    # T: time to maturity
+    # r: interest rate
+    # sigma: volatility of underlying asset
+
+    d1 = (np.log(S / K) + (r + 0.5 * sigma ** 2) * T) / (sigma * np.sqrt(T))
+    d2 = (np.log(S / K) + (r - 0.5 * sigma ** 2) * T) / (sigma * np.sqrt(T))
+
+    call = (S * si.norm.cdf(d1, 0.0, 1.0) - K *
+            np.exp(-r * T) * si.norm.cdf(d2, 0.0, 1.0))
+
+    return call
